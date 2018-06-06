bed88eb1b1999b99936538fd25a3275780ac83b6
YingdeZheng <785316237@qq.com>
2018-06-06 14:33:56 +0800

    修改cut，不传入cut，则默认50%分割

--- src/components/vueAbbr/vueAbbr.vue
+++ src/components/vueAbbr/vueAbbr.vue
@@ -10,7 +10,10 @@ export default {
     name: 'vue-abbr',
     props: {
         text: String,
-        cut: Number
+        cut: {
+            type: Number,
+            default: 0
+        }
     },
     data: function () {
         return {
@@ -22,12 +25,19 @@ export default {
     created: function () {
         let text = this.text.toString()
         let len = text.length
-        let left = (this.cut / len).toFixed(2) * 100
-        let right = 100 - left
-        this.front = text.substring(0, this.cut)
-        this.behind = text.substring(this.cut, len).split('').reverse().join('')
 
-        this.behindWidth = 'max-width:' + right + '%'
+        if (!this.cut) {
+            let mid = len % 2 === 0 ? Math.floor(len / 2) : (Math.floor(len / 2) + 1)
+            this.front = text.substring(0, mid)
+            this.behind = text.substring(mid, len).split('').reverse().join('')
+            this.behindWidth = 'max-width: 50%'
+        } else {
+            let left = (this.cut / len).toFixed(2) * 100
+            let right = 100 - left
+            this.front = text.substring(0, this.cut)
+            this.behind = text.substring(this.cut, len).split('').reverse().join('')
+            this.behindWidth = 'max-width:' + right + '%'
+        }
     }
 }
 </script>
@@ -45,11 +55,10 @@ export default {
 
   .abbre-behind {
     max-height: 24px;
-    overflow: hidden;
     text-overflow: clip;
     unicode-bidi: bidi-override;
     text-align: left;
     direction: rtl;
+    word-break: break-all;
   }
-
 </style>
